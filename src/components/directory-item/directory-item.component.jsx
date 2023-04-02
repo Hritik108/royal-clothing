42544c7d43f9abc155ccf53e67bbc2b45ab96582
Hritik chauhan <chauhanritik786@gmail.com>
2023-04-02 15:09:49 +0530

    adding redirect

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,14 @@
 // import './directory-item.styles.scss'
 import { BackgroundImage,Body,DirectoryItemContainer } from "./directory-item.styles";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({category})=>{
 
-    const {title,imageUrl}=category;
+    const {title,imageUrl,route}=category;
+    const navigate = useNavigate();
+    const onNavigationHandler = () => navigate(route);
 // console.log(imageUrl);
-    return (<DirectoryItemContainer>
+    return (<DirectoryItemContainer onClick={onNavigationHandler}>
     <BackgroundImage imageUrl={imageUrl}/>
     <Body>
       <h2>{title}</h2>
